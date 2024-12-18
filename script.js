6611e6cc6e7206108af029900da81fd714d354d5
marc <marcxtwt@gmail.com>
2024-12-18 19:16:31 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,31 @@
+const body = document.body;
+let count=0;
+if(count == 2){
+    count =0;
+}
+
+function run(){
+    if(count == 2){
+        count =0;
+    }
+    
+    console.log (count);
+
+    if(count == 0){
+        document.body.style.backgroundColor = "#15202b";
+        document.body.style.color = "white";
+        document.body.style.transition = 'background-color 0.3s ease-in';
+        
+        
+    }
+    else if(count == 1){
+        document.body.style.backgroundColor = "#f8f4f4";
+        document.body.style.color = "black";document.body.style.transition = 'background-color 0.3s ease-in';
+    }
+
+    count++;
+    console.log(count);
+}
+
+    
+
